06b0d9503abe81ecf2687e74f8dfef89ee6ea3ba
BigGoofyGuy <collin.weatherford@gmail.com>
2022-05-12 16:22:56 -0500

    Added operator functionality with single set of numbers.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 let op;
 const add = function(num1, num2) {
 	let sum = num1 + num2
-  return sum
+    return sum
 };
 
 const subtract = function(num1, num2) {
 	let diff = num1 - num2
-  return diff
+    return diff
 };
 
 const multiply = function(num1, num2) {
@@ -21,21 +21,28 @@ const division = function(num1, num2) {
 
 const operator = function(op, num1, num2) {
     if(op === '+') {
-        add(num1, num2)
+        result = add(num1, num2)
+        total.textContent = result
+        return result;
     }
     
     if(op === '-') {
-        subtract(num1, num2)
+        result = subtract(num1, num2)
+        total.textContent = result
+        return result;
     }
 
     if(op === '*') {
-        multiply(num1, num2)
+        result = multiply(num1, num2)
+        total.textContent = result
+        return result;
     }
 
     if(op === '/') {
-        division(num1, num2)
+        result = division(num1, num2)
+        total.textContent = result
     }
-
+    
 };
 
 let container = document.getElementById('container')
@@ -43,16 +50,21 @@ let container = document.getElementById('container')
 let total = document.createElement('div')
 total.classList = 'total'
 total.id = 'total'
-total.textContent = 0
-container.appendChild(total)
+let displayValue = 0;
+total.textContent = displayValue
 
+container.appendChild(total)
+let arr=[]
 for(let i = 1; i <= 9; i++){
     let buttons = document.createElement('button')
     buttons.classList = 'buttons'
     buttons.id = [i]
     buttons.textContent = [i]
     buttons.addEventListener('click', function(){
-        total.textContent = [i]
+        displayValue = [i]
+        arr.push(displayValue)
+        total.textContent = arr.join('')
+        console.log(total.textContent)
     })
     container.appendChild(buttons)
 }
@@ -61,7 +73,13 @@ let clear = document.createElement('button')
 clear.classList = 'buttons'
 clear.id = 'c'
 clear.textContent = 'c'
-
+clear.addEventListener ('click', function(){
+    console.clear()
+    displayValue = 0
+    arr = []
+    total.textContent = displayValue
+    console.log(displayValue)
+})
 container.appendChild(clear)
 
 
@@ -69,14 +87,74 @@ let zero = document.createElement('button')
 zero.classList = 'buttons'
 zero.id = '0'
 zero.textContent = '0'
+zero.addEventListener('click', function(){
+    arr.push('0')
+    total.textContent = arr.join('')
+    console.log(total.textContent)
+})
 container.appendChild(zero)
 
 let decimal = document.createElement('button')
 decimal.classList = 'buttons'
-decimal.id = '.'
+decimal.id = 'decimal'
 decimal.textContent = '.'
 decimal.addEventListener('click', function(){
-    total.textContent ='0.'
-    
 })
 container.appendChild(decimal)
+
+let plusOp = document.createElement('button')
+plusOp.classList = 'opButtons'
+plusOp.id = 'plus'
+plusOp.textContent = '+'
+plusOp.addEventListener('click', function(){
+    console.clear()
+    let temp = arr.join('')
+    num1 = parseInt(temp)
+    displayValue = 0
+    op = '+'
+    arr = []
+    total.textContent = displayValue
+})
+container.appendChild(plusOp)
+
+let subOp = document.createElement('button')
+subOp.classList = 'opButtons'
+subOp.id = 'subtract'
+subOp.textContent = '-'
+subOp.addEventListener('click', function(){
+    console.clear()
+    let temp = arr.join('')
+    num1 = parseInt(temp)
+    displayValue = 0
+    op = '-'
+    arr = []
+    total.textContent = displayValue
+})
+container.appendChild(subOp)
+
+let multiplyOp = document.createElement('button')
+multiplyOp.classList = 'opButtons'
+multiplyOp.id = 'multiply'
+multiplyOp.textContent = '*'
+multiplyOp.addEventListener('click', function(){
+    console.clear()
+    let temp = arr.join('')
+    num1 = parseInt(temp)
+    displayValue = 0
+    op = '*'
+    arr = []
+    total.textContent = displayValue
+})
+container.appendChild(multiplyOp)
+
+let equalsOp = document.createElement('button')
+equalsOp.classList = 'opButtons'
+equalsOp.id = 'equals'
+equalsOp.textContent = '='
+equalsOp.addEventListener('click', function(){
+    num2 = parseInt(arr.join(''))
+    let temp = operator(op, num1, num2)
+    num1 = temp
+    return num1;
+})
+container.appendChild(equalsOp)
